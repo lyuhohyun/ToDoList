470daf06665c751250a88325d1730fa4aaf077ff
lyuhohyun <dndb020110@gmail.com>
2021-06-02 23:44:16 +0900

    add

---
+++ function.js
@@ -0,0 +1,5 @@
+const toDoInput = document.querySelector(".todo-input");
+const toDoButton = document.querySelector(".todo-button");
+const toDoList = docuemnt.querySelector(".todo-list");
+
+toDoButton.addEventListener("click")
